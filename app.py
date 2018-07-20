a3c371810fba0be24915160718c86ad268abb03c
kisumutalent <homiemusa@gmail.com>
2018-07-20 17:54:25 +0300

    connecting to sqlite3 and retriving data

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template, redirect, url_for, request, session, flash
+from flask import Flask, render_template, redirect, url_for, request, session, flash,g
 from functools import wraps
+import sqlite3
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'refuge'
+app.database = "sample.db"
 
 def login_required(f):
 	@wraps(f)
@@ -18,7 +20,11 @@ def login_required(f):
 
 @app.route('/')
 def home():
-	return render_template('index.html')
+	g.db = connect_db()
+	cur = g.db.execute('select * from posts')
+	posts = [dict(title=row[0], description=row[1]) for row in cur.fetchall()]
+	g.db.close()
+	return render_template('index.html', posts=posts)
 
 @app.route('/welcome', methods=['GET'])
 def welcome():
@@ -43,6 +49,9 @@ def logout():
 	session.pop('logged_in', None)
 	flash('You were just Logged out')
 	return redirect(url_for('welcome'))
+
+def connect_db():
+	return sqlite3.connect(app.database)
 
 
 if __name__ == '__main__':

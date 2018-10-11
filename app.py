e15b1502f684ed26cf3aaf0f5fe1d3b3237dfa9e
kisumutalent <homiemusa@gmail.com>
2018-10-11 17:03:22 +0300

    modify app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, redirect, url_for, request, session, flash
-#from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
-from flask.ext.sqlalchemy import SQLAlchemy 
+# from flask.ext.sqlalchemy import SQLAlchemy 
 from functools import wraps
 import sqlite3
 

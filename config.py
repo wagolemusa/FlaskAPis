9e26d0359d55285919dbfcd11f05be8d753dec52
kisumutalent <homiemusa@gmail.com>
2018-10-11 17:57:23 +0300

    modify config file

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 # defualt config
 class BaseConfig(object):
 	DEBUG = False
-	SELECT_KEY = 'refuge'
+	SECRET_KEY = 'Y\x87\xc3\x87\x0c\x8c\x96rwo\x1e*\xeb\xd8/\xac\x1avw(\xbf\xe8@\xdf'
 	SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 
